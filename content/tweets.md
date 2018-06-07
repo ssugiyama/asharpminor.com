2b9c8559a6aa2651ffd54f86bbf280f987fec089
Shinichi Sugiyama <shin.sugi@gmail.com>
2018-06-08 00:38:46 +0900

    introduce new tweets page

---
+++ content/tweets.md
@@ -0,0 +1,16 @@
+---
+type: page
+title: よりぬきツイート
+author: sugi
+date: 2015-05-11
+url: /tweets
+weight: 200
+---
+
+これまでのツイートの中から個人的にお気に入りのものを集めてみた。随時更新。
+
+since 2007
+
+<div id="tweets"></div>
+
+<script src="/js/tweets.js"></script>
---
+++ src/tweets.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Tweet } from 'react-twitter-widgets'
+import TweetIds from './tweet-ids'
+
+const limit = 50;
+
+const searchParams = new URLSearchParams(location.search);
+const page = Number(searchParams.get('page')) || 1;
+
+const offset = (page - 1) * limit;
+const page_count = Math.ceil(TweetIds.length / limit);
+
+const Tweets = () => 
+      TweetIds.slice(offset, offset+limit).map(tweetId => (
+        <Tweet tweetId={tweetId} key={tweetId} />
+      ));
+
+const Pagination = () => (
+  <nav aria-label="Tweets Pagination">
+    <ul className="pagination justify-content-center flex-wrap">
+      { page > 1 ? 
+      <li className="page-item"><a className="page-link" href={ '?page=' + (page - 1) }>
+        <span aria-hidden="true">&laquo;</span>
+        <span className="sr-only">Previous</span>
+      </a></li> :
+      <li className="page-item disabled"><span className="page-link">
+        <span aria-hidden="true">&laquo;</span>
+        <span className="sr-only">Previous</span>
+      </span></li>
+      }
+      { [...Array(page_count).keys()].map(n => 
+      n+1 != page ? 
+      <li className="page-item" key={n}><a className="page-link" href={ '?page=' + (n+1)}>{n+1}</a></li> :
+      <li className="page-item active" key={n}><span className="page-link">{n+1}</span></li>
+      )}
+      { page <= page_count - 1  ?
+      <li className="page-item"><a className="page-link" href={ '?page=' + (page + 1)}>
+        <span aria-hidden="true">&raquo;</span>
+        <span className="sr-only">Next</span>
+      </a></li> :
+      <li className="page-item disabled"><span className="page-link">
+        <span aria-hidden="true">&raquo;</span>
+        <span className="sr-only">Next</span>
+      </span></li> 
+      }
+    </ul>
+  </nav>
+)
+
+ReactDOM.render(
+  <div>
+<Tweets />
+<Pagination />
+  </div>,
+  document.getElementById('tweets')
+)
+
