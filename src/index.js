41591e10beda6793c0dfe8c6d27eadaa6811482d
Shinichi SUGIYAMA <shin.sugi@gmail.com>
2021-11-03 12:11:44 +0900

    bootstrap5対応

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Import all plugins
+import * as bootstrap from 'bootstrap';
+
+// Or import only needed plugins
+import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
+
+import SocialShareKit from '../node_modules/social-share-kit/dist/js/social-share-kit.js'
+
+SocialShareKit.init();
+
+import './custom.scss';
