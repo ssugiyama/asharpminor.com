413ec2709ad8138ca3cb126340230e117b920f8e
Shinichi SUGIYAMA <shin.sugi@gmail.com>
2022-11-24 02:32:35 +0900

    social-share-kit削除

--- src/index.js
+++ src/index.js
@@ -3,9 +3,4 @@ import * as bootstrap from 'bootstrap';
 
 // Or import only needed plugins
 import { Tooltip as Tooltip, Toast as Toast, Popover as Popover } from 'bootstrap';
-
-import '../node_modules/social-share-kit/dist/js/social-share-kit.js';
-
-SocialShareKit.init();
-
 import './custom.scss';
